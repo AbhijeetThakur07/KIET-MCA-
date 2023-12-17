beba83a22045d924db613b003754cf9e34838c1e
Abhijeet Singh <abhithakur8808@gmail.com>
2023-12-18 01:10:07 +0530

    Create Average.js

---
+++ Average.js
@@ -0,0 +1,10 @@
+function average(arr){
+let sumofelement=0;
+let totalnoOfelements=5;
+for(i=1;i<=totalnoOfelements;i++){
+sumofelements+=arr[i]
+ }
+return average=sumofelements/totalnoOfelements
+
+}
+document.write('average:',average([1,,2,3,4,5]))
