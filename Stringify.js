19a25eea3f4d0d14b514b738dde8adc60e20e1b5
Abhijeet Singh <abhithakur8808@gmail.com>
2023-12-17 22:20:08 +0530

    Create Stringify.js

---
+++ Stringify.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Create a JSON string from a Javascript object.</h2>
+<p id="demo"></p>
+
+<script>
+const obj={"name":"KIET MCA","age":30,"city":"GHAZIABAD"};
+const myJSON=JSON.stringify(obj);
+document.getElementById("demo").innerHTML=myJSON;
+</script>
+</body>
+</html>
