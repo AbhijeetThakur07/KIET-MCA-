9b2660e1fd007a08a183d985151b464b78edac19
Abhijeet Singh <abhithakur8808@gmail.com>
2023-12-18 01:14:51 +0530

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,13 @@
+let obj={
+ "name":"Abhijeet",
+ "age":21,
+ "Course":"MCA"
+ }
+//for in loop
+for(let ax in obj){
+document.write(" Detail " +ax+ " is "+obj[ax])
+}
+//for of loop
+ for(let bx of "name"){
+ document.write(bx)
+}
